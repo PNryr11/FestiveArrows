8d6bff39c2cffcf2c793a2f0a865a3016f5db6e1
Isaac Moore <rmsy@me.com>
2013-07-04 13:25:07 -0500

    Add support for dispensers, bump to 1.0.1-SNAPSHOT

--- src/main/java/me/yukonapplegeek/festivearrows/FestiveArrows.java
+++ src/main/java/me/yukonapplegeek/festivearrows/FestiveArrows.java
@@ -5,14 +5,12 @@ import org.bukkit.FireworkEffect;
 import org.bukkit.FireworkEffect.Type;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.entity.Arrow;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Firework;
-import org.bukkit.entity.Projectile;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockDispenseEntityEvent;
 import org.bukkit.event.entity.EntityShootBowEvent;
 import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.inventory.ItemStack;
@@ -21,6 +19,20 @@ import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FestiveArrows extends JavaPlugin implements Listener {
+
+    private static void attachFirework(Arrow arrow) {
+        FireworkMeta fireworkMeta = (FireworkMeta) (new ItemStack(Material.FIREWORK)).getItemMeta();
+        Location location = arrow.getLocation();
+        Firework firework = (Firework) location.getWorld().spawnEntity(location, EntityType.FIREWORK);
+
+        fireworkMeta.addEffect(FireworkEffect.builder()
+                .with(Type.BURST)
+                .withColor(Color.RED).withColor(Color.WHITE).withColor(Color.BLUE)
+                .withTrail()
+                .build());
+        firework.setFireworkMeta(fireworkMeta);
+        arrow.setPassenger(firework);
+    }
 
     @Override
     public void onDisable() {
@@ -35,18 +47,10 @@ public class FestiveArrows extends JavaPlugin implements Listener {
     @EventHandler
     public void onBowShoot(EntityShootBowEvent event) {
         Projectile projectile = (Projectile) event.getProjectile();
-        FireworkMeta fireworkMeta = (FireworkMeta) (new ItemStack(Material.FIREWORK)).getItemMeta();
-        Firework firework = (Firework) projectile.getLocation().getWorld().spawnEntity(projectile.getLocation(), EntityType.FIREWORK);
-
-        fireworkMeta.addEffect(FireworkEffect.builder()
-                .with(Type.BURST)
-                .withColor(Color.RED).withColor(Color.WHITE).withColor(Color.BLUE)
-                .withTrail()
-                .build());
-        firework.setFireworkMeta(fireworkMeta);
-        projectile.setPassenger(firework);
-
-        projectile.setMetadata("festivearrows.hasLanded", new FixedMetadataValue(this, new Boolean(false)));
+        if (projectile instanceof Arrow) {
+            FestiveArrows.attachFirework((Arrow) projectile);
+            projectile.setMetadata("festivearrows.hasLanded", new FixedMetadataValue(this, new Boolean(false)));
+        }
     }
 
     @EventHandler(priority = EventPriority.MONITOR)
@@ -60,6 +64,15 @@ public class FestiveArrows extends JavaPlugin implements Listener {
                 projectile.removeMetadata("festivearrows.hasLanded", this);
                 projectile.setMetadata("festivearrows.hasLanded", new FixedMetadataValue(this, new Boolean(true)));
             }
+        }
+    }
+
+    @EventHandler
+    public void onDispenseEntity(BlockDispenseEntityEvent event) {
+        Entity entity = event.getEntity();
+        if (entity instanceof Arrow) {
+            FestiveArrows.attachFirework((Arrow) entity);
+            entity.setMetadata("festivearrows.hasLanded", new FixedMetadataValue(this, new Boolean(false)));
         }
     }
 
